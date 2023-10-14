157a979583c8bdcad2feed1300a867e2c7253ed5
pmendozam <pmendozam3@miumg.edu.gt>
2023-10-13 18:17:37 -0600

    cambio

--- datos.php
+++ datos.php
@@ -5,7 +5,7 @@ $pdo = new Conexion();
 
 //Listar registros y consultar registro
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	$sql = $pdo->prepare("SELECT * FROM grado  WHERE id=:id");
+	$sql = $pdo->prepare("SELECT * FROM grado ");
 	$sql->execute();
 	$sql->setFetchMode(PDO::FETCH_ASSOC);
 	header("HTTP/1.1 200 hay datos");
